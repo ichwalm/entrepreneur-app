fd9f2fff8e76438d183f5596191f75082017f13e
ichwalM <ichwal093@gmail.com>
2026-05-12 10:50:59 +0800

    new pull

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Wajib jika web kamu menampilkan gambar dari luar (seperti Vercel Blob, AWS S3, dll)
+  images: {
+    domains: ['blob.vercel-storage.com'], 
+  },
+  
+  // Sangat disarankan dinyalakan agar Next.js lebih ketat mengecek error
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
